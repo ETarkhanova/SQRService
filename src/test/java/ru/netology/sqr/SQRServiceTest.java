22e4e91525055294848aad8e80d283ea89c71460
ETarkhanova <89924219+ETarkhanova@users.noreply.github.com>
2021-10-23 12:16:08 +0500

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,5 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,14 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"numberOfSquares,99,3"})
-    void shouldNumberOfSquares(String testName, int number, int expected) {
+    @CsvSource(value = {"checking inside range limit, 200, 300, 3",
+            "checking lower range limit, 200, 200, 0",
+            "checking under lower range limit, 100, 100, 1",
+            "checking upper range limit, 300, 300, 0",
+            "checking over upper range limit, 400, 400, 1",
+            "checking all number limit, 100, 9801, 90"})
+    void shouldNumberOfSquares(String testName, int minLimit, int maxLimit, int expected) {
         SQRService service = new SQRService();
 
-        //int number = 99;
-        //int expected = 3;
-
-        int actual = service.numberOfSquares(number);
+        int actual = service.numberOfSquares(minLimit, maxLimit);
 
         assertEquals(expected, actual);
 
